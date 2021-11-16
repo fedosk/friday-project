54f71f87242eb9e09616bc9895cfda412db02c1f
Noak-Sergey <noakstuff@gmail.com>
2021-11-16 23:32:37 +0300

    feat: implement registerAPI, registerReducer, setEmailPasswordRegister-action creator, sendRegisterDataTC, types.

--- src/features/feature1-auth/Register/register-reduser.ts
+++ src/features/feature1-auth/Register/register-reduser.ts
@@ -1,12 +1,56 @@
-const initialState = {}
+import axios from "axios";
+import {Dispatch} from "redux";
+
+
+const instance = axios.create({
+    baseURL: 'http://localhost:7542/2.0/'
+})
+
+//api
+export const registerAPI = {
+    registerUser(email: string, password: string) {
+        return instance.post(`auth/register`, {email, password})
+    }
+}
+
+
+const initialState = {
+    email: '',
+    password: ''
+}
 
 type InitialStateType = typeof initialState
 
 export const registerReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-    return state
+    switch (action.type) {
+        case "REGISTER/SET-EMAIL-PASSWORD": {
+            return {...state, email: action.email, password: action.password}
+        }
+        default:
+            return state
+    }
+}
+
+//actions
+export const setEmailPasswordRegister = (email: string, password:string) => {
+    return {
+        type: 'REGISTER/SET-EMAIL-PASSWORD',
+        email,
+        password
+    } as const
 }
 
 // thunks
+export const sendRegisterDataTC = (email: string, password: string) => {
+    return (dispatch: Dispatch) => {
+        registerAPI.registerUser(email, password)
+            .then((res) => {
+                dispatch(setEmailPasswordRegister(email,password))
+            })
+    }
+}
 
 // types
-type ActionsType = any
+type SetEmailRegisterType = ReturnType<typeof setEmailPasswordRegister>
+
+type ActionsType = SetEmailRegisterType
