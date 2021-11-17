db143bc158048889e667b89c0906b89c6a9d3378
Larionov Anton <larrianton@gmail.com>
2021-11-17 18:42:14 +0200

    feet : add HandlerHook to delete duplicate code

---
+++ src/hooks/newPasswordHandler/newPasswordHandler.ts
@@ -0,0 +1,34 @@
+import {useDispatch} from "react-redux";
+import {Dispatch, useState} from "react";
+import {SetNewPasswordType} from "../../features/feature1-auth/password-recovery/password-recovery-reduser";
+
+type ThunkType = (dispatch: Dispatch<any>) => void
+type useNewPasswordHandlerType = {
+    action: (newPassword: string) => SetNewPasswordType,
+    thunk: (token: string, newPassword: string) => ThunkType,
+    newPassword: string
+    token: string
+}
+export const useNewPasswordHandler = ({
+                                          action,
+                                          thunk,
+                                          newPassword,
+                                          token,
+                                      }: useNewPasswordHandlerType) => {
+    const [newPasswordError, setNewPasswordError] = useState<string>('');
+    const dispatch = useDispatch()
+
+    const onChange = (newPassword: string) => {
+        setNewPasswordError('');
+        dispatch(action(newPassword));
+    }
+    const onClick = () => {
+        if (!newPassword) {
+            setNewPasswordError('Введите пароль')
+        } else if (newPassword.length < 4) {
+            setNewPasswordError('Пароль не менее 4 символов');
+        }
+        dispatch(thunk(token, newPassword));
+    }
+    return {onChange, onClick, newPasswordError}
+}
---
+++ src/hooks/passwordRecoveryHandler/passwordRecoveryHandler.ts
@@ -0,0 +1,32 @@
+import {useDispatch} from "react-redux";
+import {Dispatch, useState} from "react";
+import {SetEmailType} from "../../features/feature1-auth/password-recovery/password-recovery-reduser";
+
+type ThunkType = (dispatch: Dispatch<any>) => void
+type usePasswordRecoveryHandlerType = {
+    action: (email: string) => SetEmailType,
+    thunk: (email: string) => ThunkType,
+    email: string
+}
+export const usePasswordRecoveryHandler = ({
+                                               action,
+                                               thunk,
+                                               email,
+                                           }: usePasswordRecoveryHandlerType) => {
+    const [emailError, setEmailError] = useState<string>('');
+    const dispatch = useDispatch()
+
+    const onChange = (email: string) => {
+        setEmailError('');
+        dispatch(action(email));
+    }
+    const onClick = () => {
+        if (!email) {
+            setEmailError('Введите адрес')
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+            setEmailError('Неправильный адрес');
+        }
+        dispatch(thunk(email));
+    }
+    return {onChange, onClick, emailError}
+}
