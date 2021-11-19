c952d46506063ced2fe3d5fe90a982cc6a023ae2
Eduard Fedosevich <fedosewiched@gmail.com>
2021-11-19 18:23:07 +0300

    refactor: change url api in register

--- src/features/feature1-auth/Register/register-reduser.ts
+++ src/features/feature1-auth/Register/register-reduser.ts
@@ -4,7 +4,7 @@ import {Dispatch} from "redux";
 
 //api
 const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/'
+    baseURL: 'https://neko-back.herokuapp.com/2.0'
 })
 
 export const registerAPI = {
